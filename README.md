ef09f96231c83fa26547717d6378f26de16484e5
TheAustrian1998 <theaustrianeth@gmail.com>
2021-09-19 11:56:41 -0300

    Updated README, added forestV2 task

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ### theRarityForest
 
-address: [FTMScan](https://ftmscan.com/address/0xb37d3d79ea86B0334d9322c695339D577A3D57be#code)
+address forestV1: [FTMScan](https://ftmscan.com/address/0xb37d3d79ea86B0334d9322c695339D577A3D57be#code) (If you are here, you can migrate to V2 for free)
+
+address forestV2: [FTMScan](https://ftmscan.com/address/0x9e894cd5dcc5bad1ed3663077871d9d010f654b5#code)
 
 
 
@@ -11,4 +13,8 @@ address: [FTMScan](https://ftmscan.com/address/0xb37d3d79ea86B0334d9322c695339D5
 -Wait the time in days you entered (example: 7 days). Call `discover` and you will receive a treasure.
 
 -You can level up your treasure using your XP, calling `levelUp`.
+
+
+
+I recommend playing it at [rarityExtended UI](https://rarityextended.com/)
 
--- tasks/treasuresV2.js
+++ tasks/treasuresV2.js
@@ -1,5 +1,58 @@
 const { task } = require("hardhat/config");
 
 task("treasuresV2", "Groups quantity by treasures (V2)", async (_, { ethers }) => {
-    //Do something
+
+    //Sort a dict
+    function sortDict(dict) {
+        // Create items array
+        let items = Object.keys(dict).map(function (key) {
+            return [key, dict[key]];
+        });
+
+        // Sort the array based on the second element
+        items.sort(function (first, second) {
+            return second[1] - first[1];
+        });
+
+        return items;
+    }
+
+    const forestV2Addr = "0x9e894cd5dcc5bad1ed3663077871d9d010f654b5";
+
+    //Attach to contract on fantom mainnet
+    let TheRarityForestV2 = await ethers.getContractFactory("TheRarityForestV2");
+    //Query total quantity
+    let quant = ethers.utils.formatUnits(await TheRarityForestV2.attach(forestV2Addr)._tokenIdCounter(), "wei");
+    console.log("total: ", quant);
+
+    //Filter all items
+    let items = {};
+    let summonners = {};
+    for (let i = 0; i < quant; i++) {
+        let treasure = await TheRarityForestV2.attach(forestV2Addr).treasure(i);
+
+        if (items[treasure._itemName]) {
+            items[treasure._itemName] += 1;
+        } else {
+            items[treasure._itemName] = 1;
+        }
+
+        if (summonners[treasure._summonerId]) {
+            summonners[treasure._summonerId] += 1;
+        } else {
+            summonners[treasure._summonerId] = 1;
+        }
+
+        process.stdout.write(`querying treasures... ${Math.trunc(i * 100 / quant)}% \r`);
+
+    }
+
+    //Print
+    console.log("\n");
+    let sortedItems = sortDict(items);
+    console.log(sortedItems);
+
+    console.log("\n");
+    let sortedSummoners = sortDict(summonners);
+    console.log(sortedSummoners);
 });
