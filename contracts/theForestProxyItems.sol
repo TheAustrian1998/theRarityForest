37e0c23d7d9d76b2fc722dba38cd9969ce46c457
TheAustrian1998 <theaustrianeth@gmail.com>
2021-10-06 19:52:50 -0300

    Beautify theForestProxyItems.sol

--- contracts/theForestProxyItems.sol
+++ contracts/theForestProxyItems.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
-interface IForest {
-    function treasure(uint tokenId) external view returns (uint256 _summonerId, string memory _itemName, uint _magic, uint _level);
-}
+import "./interfaces/ITheRarityForestV3.sol";
 
-contract theForest_proxy_items {
-    IForest constant _forest = IForest(0x48e6F88F1Ab05677675dE9d14a705f8A137ea2bC);
+contract theForestProxyItems {
+    ITheRarityForestV3 constant _forest = ITheRarityForestV3(0x48e6F88F1Ab05677675dE9d14a705f8A137ea2bC);
 
     struct item {
         uint8 base_type; // 1 for goods, 2 for armor, 3 for weapon, 4 for jewelries
@@ -38,9 +36,10 @@ contract theForest_proxy_items {
         return false;
     }
 
-    function    items(uint _itemID) public view returns (item memory) {
+    function items(uint _itemID) public view returns (item memory) {
         (,string memory _itemName,,) = _forest.treasure(_itemID);
 
+		//Goods
 		if (compareStrings(_itemName, "Dead King crown")) return (item(1, 1));
 		if (compareStrings(_itemName, "Ancient book")) return (item(1, 2));
 		if (compareStrings(_itemName, "Enchanted book")) return (item(1, 3));
@@ -66,7 +65,7 @@ contract theForest_proxy_items {
 		if (compareStrings(_itemName, "Metal horse saddle")) return (item(1, 23));
 		if (compareStrings(_itemName, "Witcher book")) return (item(1, 24));
 		if (compareStrings(_itemName, "Witch book")) return (item(1, 25));
-		if (compareStrings(_itemName, "Unknown animal ey")) return (item(1, 26));
+		if (compareStrings(_itemName, "Unknown animal eye")) return (item(1, 26));
 		if (compareStrings(_itemName, "Shadowy rabbit paw")) return (item(1, 27));
 		if (compareStrings(_itemName, "Red Tanned Gloves")) return (item(1, 28));
 		if (compareStrings(_itemName, "Paladin heart")) return (item(1, 29));
@@ -76,10 +75,12 @@ contract theForest_proxy_items {
 		if (compareStrings(_itemName, "Meteorite fragment")) return (item(1, 33));
 		if (compareStrings(_itemName, "Mutant fisheye")) return (item(1, 34));
 
+		//Armors
 		if (compareStrings(_itemName, "Haunted cloak")) return (item(1, 1));
 		if (compareStrings(_itemName, "Dead hero cape")) return (item(1, 2));
 		if (compareStrings(_itemName, "Slain warrior armor")) return (item(1, 3));
 
+		//Weapons
         if (compareStrings(_itemName, "Black gauntlet")) return (item(3, 1));
         if (compareStrings(_itemName, "Silver sword")) return (item(3, 2));
         if (compareStrings(_itemName, "Ancient Prince Andre's Sword")) return (item(3, 3));
@@ -88,6 +89,7 @@ contract theForest_proxy_items {
         if (compareStrings(_itemName, "Thunder hammer")) return (item(3, 6));
         if (compareStrings(_itemName, "Old farmer sickle")) return (item(3, 7));
 
+		//Jewelry
 		if (compareStrings(_itemName, "Haunted ring")) return (item(4,1));
 		if (compareStrings(_itemName, "Gold ring")) return (item(4,2));
 		if (compareStrings(_itemName, "Magic necklace")) return (item(4,3));
@@ -99,6 +101,7 @@ contract theForest_proxy_items {
 		if (compareStrings(_itemName, "Time crystal")) return (item(4,9));
 		if (compareStrings(_itemName, "Antique ring")) return (item(4,10));
 		if (compareStrings(_itemName, "Wolf necklace")) return (item(4,11));
+		
         return item(0, 0);
     }
 	
